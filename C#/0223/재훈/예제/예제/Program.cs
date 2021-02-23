3809d4efdbab6168c3e4c2aebdb1b45ce0a9417f
jacksimuse <cjh930306@naver.com>
2021-02-24 01:00:05 +0900

    0223 예제

---
+++ C#/0223/재훈/예제/예제/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 예제
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            string yoonnyeon = Console.ReadLine();
+
+            int number = 0;
+            int.TryParse(yoonnyeon, out number);
+            if (number % 4 == 0)
+            {
+                if ((number % 100 != 0) || (number % 400 == 0))
+                    Console.WriteLine("1");
+            }
+            else
+                Console.WriteLine("0");
+                
+        }
+    }
+}
---
+++ C#/0223/재훈/예제/예제2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 예제2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100 ; i++)
+            {
+                for (int j = 0; j < 100 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
