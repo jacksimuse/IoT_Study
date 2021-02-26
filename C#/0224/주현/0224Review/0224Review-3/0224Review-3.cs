40f58ff4d703e14f5f9c961aa3a748cdb8d2cc3a
joohy <joohyun104@gmail.com>
2021-02-26 09:34:42 +0900

    0226 9:34

---
+++ C#/0224/주현/0224Review/0224Review-3/0224Review-3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0224Review_3
+{
+    class Program
+    {
+        #region 문제
+        /*
+        요세푸스 문제는 다음과 같다.
+        1번부터 N번까지 N명의 사람이 원을 이루면서 앉아있고, 양의 정수 K(≤ N)가 주어진다. 
+        이제 순서대로 K번째 사람을 제거한다. 한 사람이 제거되면 남은 사람들로 이루어진 원을 따라 이 과정을 계속해 나간다. 
+        이 과정은 N명의 사람이 모두 제거될 때까지 계속된다. 원에서 사람들이 제거되는 순서를 (N, K)-요세푸스 순열이라고 한다. 
+        예를 들어 (7, 3)-요세푸스 순열은 <3, 6, 2, 7, 5, 1, 4>이다.
+        N과 K가 주어지면 (N, K)-요세푸스 순열을 구하는 프로그램을 작성하시오.
+
+        첫째 줄에 N과 K가 빈 칸을 사이에 두고 순서대로 주어진다. (1 ≤ K ≤ N ≤ 1,000)
+        예제와 같이 요세푸스 순열을 출력한다.
+
+         */
+        #endregion
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
