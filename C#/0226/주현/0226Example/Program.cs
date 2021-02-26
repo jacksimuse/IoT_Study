acb8990a63801ad1469af7feabd900ee457c569f
joohy <joohyun104@gmail.com>
2021-02-26 09:38:49 +0900

    0226 9:38 주현

---
+++ C#/0226/주현/0226Example/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0226Example
+{
+    /*
+     * 0. 주소 입력 
+     * 1. 주소 검색
+     * 2. 주소 수정
+     * 3. 주소 삭제
+     * 4. 주소 전체 출력
+     * 5. 프로그램 종료
+     * 메뉴를 선택하세요 >>>
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
