6f9961ec433c51a5b522c6e2a2f278ab2e095fc7
jacksimuse <cjh930306@naver.com>
2021-03-09 00:52:55 +0900

    계산기 커밋

---
+++ C#/0308/재훈/Jaehun_Cal/Jaehun_Cal/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Jaehun_Cal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtNum1 = new System.Windows.Forms.TextBox();
+            this.TxtNum2 = new System.Windows.Forms.TextBox();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.BtnPlus = new System.Windows.Forms.Button();
+            this.BtnMinus = new System.Windows.Forms.Button();
+            this.BtnMultiple = new System.Windows.Forms.Button();
+            this.BtnDivision = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Num1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Num2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Result";
+            // 
+            // TxtNum1
+            // 
+            this.TxtNum1.Location = new System.Drawing.Point(89, 18);
+            this.TxtNum1.Name = "TxtNum1";
+            this.TxtNum1.Size = new System.Drawing.Size(222, 21);
+            this.TxtNum1.TabIndex = 3;
+            // 
+            // TxtNum2
+            // 
+            this.TxtNum2.Location = new System.Drawing.Point(89, 62);
+            this.TxtNum2.Name = "TxtNum2";
+            this.TxtNum2.Size = new System.Drawing.Size(222, 21);
+            this.TxtNum2.TabIndex = 4;
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(89, 102);
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(234, 21);
+            this.TxtResult.TabIndex = 5;
+            // 
+            // BtnPlus
+            // 
+            this.BtnPlus.Location = new System.Drawing.Point(337, 17);
+            this.BtnPlus.Name = "BtnPlus";
+            this.BtnPlus.Size = new System.Drawing.Size(37, 21);
+            this.BtnPlus.TabIndex = 6;
+            this.BtnPlus.Text = "+";
+            this.BtnPlus.UseVisualStyleBackColor = true;
+            this.BtnPlus.Click += new System.EventHandler(this.BtnPlus_Click);
+            // 
+            // BtnMinus
+            // 
+            this.BtnMinus.Location = new System.Drawing.Point(400, 18);
+            this.BtnMinus.Name = "BtnMinus";
+            this.BtnMinus.Size = new System.Drawing.Size(37, 21);
+            this.BtnMinus.TabIndex = 7;
+            this.BtnMinus.Text = "-";
+            this.BtnMinus.UseVisualStyleBackColor = true;
+            this.BtnMinus.Click += new System.EventHandler(this.BtnMinus_Click);
+            // 
+            // BtnMultiple
+            // 
+            this.BtnMultiple.Location = new System.Drawing.Point(337, 62);
+            this.BtnMultiple.Name = "BtnMultiple";
+            this.BtnMultiple.Size = new System.Drawing.Size(37, 21);
+            this.BtnMultiple.TabIndex = 8;
+            this.BtnMultiple.Text = "*";
+            this.BtnMultiple.UseVisualStyleBackColor = true;
+            this.BtnMultiple.Click += new System.EventHandler(this.BtnMultiple_Click);
+            // 
+            // BtnDivision
+            // 
+            this.BtnDivision.Location = new System.Drawing.Point(400, 61);
+            this.BtnDivision.Name = "BtnDivision";
+            this.BtnDivision.Size = new System.Drawing.Size(37, 21);
+            this.BtnDivision.TabIndex = 9;
+            this.BtnDivision.Text = "/";
+            this.BtnDivision.UseVisualStyleBackColor = true;
+            this.BtnDivision.Click += new System.EventHandler(this.BtnDivision_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 150);
+            this.Controls.Add(this.BtnDivision);
+            this.Controls.Add(this.BtnMultiple);
+            this.Controls.Add(this.BtnMinus);
+            this.Controls.Add(this.BtnPlus);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.TxtNum2);
+            this.Controls.Add(this.TxtNum1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "계산기";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtNum1;
+        private System.Windows.Forms.TextBox TxtNum2;
+        private System.Windows.Forms.TextBox TxtResult;
+        private System.Windows.Forms.Button BtnPlus;
+        private System.Windows.Forms.Button BtnMinus;
+        private System.Windows.Forms.Button BtnMultiple;
+        private System.Windows.Forms.Button BtnDivision;
+    }
+}
+
---
+++ C#/0308/재훈/Jaehun_Cal/Jaehun_Cal/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jaehun_Cal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Calculator(char type)
+        {
+            double Num1 = double.Parse(TxtNum1.Text.ToString());
+            double Num2 = double.Parse(TxtNum2.Text.ToString());
+            double Result = 0;
+
+            try
+            {
+                if (type == '+')
+                {
+                    Result = Num1 + Num2;
+
+                    TxtResult.Text = Result.ToString();
+                }
+                else if (type == '-')
+                {
+                    Result = Num1 - Num2;
+
+                    TxtResult.Text = Result.ToString();
+                }
+                else if (type == '*')
+                {
+                    Result = Num1 * Num2;
+
+                    TxtResult.Text = Result.ToString();
+                }
+                else
+                {
+                    Result = Num1 / Num2;
+
+                    TxtResult.Text = Result.ToString();
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                MessageBox.Show($"예외발생 { ex.Message}");
+            }
+        }
+        
+        private void BtnPlus_Click(object sender, EventArgs e)
+        {
+            Calculator('+');
+        }
+
+        private void BtnMinus_Click(object sender, EventArgs e)
+        {
+            Calculator('-');
+        }
+
+        private void BtnMultiple_Click(object sender, EventArgs e)
+        {
+            Calculator('*');
+        }
+
+        private void BtnDivision_Click(object sender, EventArgs e)
+        {
+            Calculator('/');
+        }
+    }
+}
