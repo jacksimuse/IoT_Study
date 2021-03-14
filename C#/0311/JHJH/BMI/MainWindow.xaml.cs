ed3bd12dfa11391e386c606bbba29911570ad9e1
jacksimuse <cjh930306@naver.com>
2021-03-14 16:31:42 +0900

    예외처리까지 추가

--- C#/0311/JHJH/BMI/MainWindow.xaml.cs
+++ C#/0311/JHJH/BMI/MainWindow.xaml.cs
@@ -27,41 +27,49 @@ namespace BMI
 
         private void BtnCalc_Click(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(TxtHeight.Text) || string.IsNullOrEmpty(TxtWeight.Text))
+            try
             {
-                TxtResult.Text = "키와 몸무게를 입력하세요.";
-                return;
-            }
+                if (string.IsNullOrEmpty(TxtHeight.Text) || string.IsNullOrEmpty(TxtWeight.Text))
+                {
+                    TxtResult.Text = "키와 몸무게를 입력하세요.";
+                    return;
+                }
 
-            double height = double.Parse(TxtHeight.Text) / 100.0;
-            double weight = double.Parse(TxtWeight.Text);
-            double bmi = weight / (height * height);
-            TxtResult.Text = $"당신의 BMI는 {bmi:F2} 입니다.";
+                double height = double.Parse(TxtHeight.Text) / 100.0;
+                double weight = double.Parse(TxtWeight.Text);
+                double bmi = weight / (height * height);
+                TxtResult.Text = $"당신의 BMI는 {bmi:F2} 입니다.";
 
-            if (bmi >= 35)
-            {
-                TxtResult.Text = "고도 비만 운동 ㄱㄱ";
+                if (bmi >= 35)
+                {
+                    TxtResult.Text = $"당신의 BMI는 { bmi: F2}으로 고도 비만입니다 운동 ㄱㄱ";
+                }
+                else if (bmi >= 30 && bmi <= 34.9)
+                {
+                    TxtResult.Text = $"당신의 BMI는 { bmi: F2}으로 중(重)도 비만(2단계 비만) 입니다";
+                }
+                else if (bmi >= 25 && bmi <= 29.9)
+                {
+                    TxtResult.Text = $"당신의 BMI는 { bmi: F2}으로 경도 비만(1단계 비만) 입니다";
+                }
+                else if (bmi >= 23 && bmi <= 24.9)
+                {
+                    TxtResult.Text = $"당신의 BMI는 { bmi: F2}으로 과체중입니다 ";
+                }
+                else if (bmi >= 18.5 && bmi <= 22.9)
+                {
+                    TxtResult.Text = $"당신의 BMI는 { bmi: F2}으로 정상입니다";
+                }
+                else if (bmi >= 0 && bmi <= 18.5)
+                {
+                    TxtResult.Text = $"당신의 BMI는 { bmi: F2}으로 저체중입니다";
+                }
             }
-            else if (bmi >= 30 && bmi <= 34.9)
+            catch (Exception)
             {
-                TxtResult.Text = "중(重)도 비만(2단계 비만)";
+                MessageBox.Show("숫자로 입력해주세요 ㅎㅎ");
             }
-            else if (bmi >= 25 && bmi <= 29.9)
-            {
-                TxtResult.Text = "경도 비만(1단계 비만)";
-            }
-            else if (bmi >= 23 && bmi <= 24.9)
-            {
-                TxtResult.Text = "과체중";
-            }
-            else if (bmi >= 18.5 && bmi <= 22.9)
-            {
-                TxtResult.Text = "정상";
-            }
-            else if (bmi >= 0 && bmi <= 18.5)
-            {
-                TxtResult.Text = "저체중";
-            }
+            
 
         }
     }
