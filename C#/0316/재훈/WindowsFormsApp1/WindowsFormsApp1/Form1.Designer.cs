3e215154be0d83218c7a02042a26b5799211b50c
jacksimuse <cjh930306@naver.com>
2021-03-17 23:40:37 +0900

    0316 커밋

---
+++ C#/0316/재훈/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(62, 64);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(282, 215);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(38, 12);
+            this.lblMsg.TabIndex = 2;
+            this.lblMsg.Text = "label1";
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(62, 158);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(100, 23);
+            this.progressBar2.TabIndex = 3;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(62, 254);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(100, 23);
+            this.progressBar3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+    }
+}
+
---
+++ C#/0316/재훈/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private BackgroundWorker worker;
+
+        private Timer timer;
+        private int timerCount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+            worker.DoWork += new DoWorkEventHandler(worker_DoWork);
+            worker.ProgressChanged += new ProgressChangedEventHandler(worker_ProgressChanged);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(worker_RunWorkerCompleted);
+
+            // 테스트용 타이머 이용
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(timer_Tick);
+        }
+
+        
+
+        // Worker Thread가 실제 하는 일
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string srcDir = @"C:\Temp\_Src";
+            string destDir = @"C:\Temp\_Dest";
+
+            DirectoryInfo di = new DirectoryInfo(srcDir);
+            FileInfo[] fileInfos = di.GetFiles();
+            int totalFiles = fileInfos.Length;
+            int counter = 0;
+            int pct = 0;
+            foreach (var fi in fileInfos)
+            {
+                string destFile = Path.Combine(destDir, fi.Name);
+                File.Copy(fi.FullName, destFile);
+
+                pct = ((++counter * 100) / totalFiles);
+                worker.ReportProgress(pct);
+            }
+        }
+
+        // Progress 리포트 - UI Thread
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.progressBar1.Value = e.ProgressPercentage;
+        }
+
+        // 작업 완료 - UI Thread
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // 에러가 있는지 체크
+            if (e.Error != null)
+            {
+                lblMsg.Text = e.Error.Message;
+                MessageBox.Show(e.Error.Message, "Error");
+                return;
+            }
+            lblMsg.Text = "성공적으로 완료되었습니다";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 비동기(Async)로 진행
+            worker.RunWorkerAsync();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 디폴트값 사용 (Maximum = 100, Minimum = 0, Step = 10)
+            progressBar2.Style = ProgressBarStyle.Continuous;
+            progressBar2.Minimum = 0;
+            progressBar2.Maximum = 90;
+            progressBar2.Step = 5;
+            progressBar2.Value = 0;
+
+            // Marquee 스타일
+            progressBar3.Style = ProgressBarStyle.Marquee;
+            progressBar3.Enabled = true;
+
+            // 테스트를 위해 타이머 시작
+            timer.Start();
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            // 한 스텝 이동
+            progressBar1.PerformStep();
+            progressBar2.PerformStep();
+
+            // 타이머 중지 조건
+            if (++timerCount == 10)
+            {
+                timer.Stop();
+                progressBar3.Enabled = false;
+            }
+        }
+
+    }
+}
