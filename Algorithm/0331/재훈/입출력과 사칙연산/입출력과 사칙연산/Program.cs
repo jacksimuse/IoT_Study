2893eaf0357b0e2261cac4dcf5e5d9425e92c057
jacksimuse <cjh930306@naver.com>
2021-04-01 16:42:10 +0900

    커컥커커컥mit

---
+++ Algorithm/0331/재훈/입출력과 사칙연산/입출력과 사칙연산/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace 입출력과_사칙연산
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("세자리 숫자를 입력하시오");
+            Console.WriteLine("세자리 숫자를 입력하시오");
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            int c = int.Parse(a);
+            int d = int.Parse(b);
+
+            
+        }
+    }
+}
