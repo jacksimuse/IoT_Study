15f1ce362caf45378e47cfb0029cf2d9a328144a
jacksimuse <cjh930306@naver.com>
2021-04-01 23:54:54 +0900

    입출력과 사칙연산

---
+++ Algorithm/0331/재훈/입출력과 사칙연산/나머지/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 나머지
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 숫자 3개를 받아온다
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(); // 한 줄 띄우기용
+
+            // 그대로 표현했다..
+            Console.WriteLine((a + b) % c);
+            Console.WriteLine((a % c) + (b % c) % c);
+            Console.WriteLine((a * b) % c);
+            Console.WriteLine((a % c) * (b % c) % c);
+        }
+    }
+}
--- Algorithm/0331/재훈/입출력과 사칙연산/입출력과 사칙연산/Program.cs
+++ Algorithm/0331/재훈/입출력과 사칙연산/입출력과 사칙연산/Program.cs
@@ -6,14 +6,30 @@ namespace 입출력과_사칙연산
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("세자리 숫자를 입력하시오");
-            Console.WriteLine("세자리 숫자를 입력하시오");
+            // 세자리 숫자를 받아온다 
             string a = Console.ReadLine();
             string b = Console.ReadLine();
-            int c = int.Parse(a);
-            int d = int.Parse(b);
 
+            // 첫 세자리 숫자
+            int c = int.Parse(a);
             
+            // 두번째 세자리 숫자를 각 자리마다 나눠준다
+            int d = int.Parse(b.Substring(2));
+            int e = int.Parse(b.Substring(1,1));
+            int f = int.Parse(b.Substring(0,1));
+
+            // 뒤에서부터 곱해준다
+            int third = c * d; // 세자리 곱하기 1의자리
+            int second = c * (e * 10); // 세자리 곱하기 10의 자리
+            int first = c * (f * 100) ; // 세자리 곱하기 100의자리
+
+            int total = first + second + third; 
+            Console.WriteLine(third);
+            Console.WriteLine(second);
+            Console.WriteLine(first);
+
+            Console.WriteLine(total); // 각 자리 합
+
         }
     }
 }
