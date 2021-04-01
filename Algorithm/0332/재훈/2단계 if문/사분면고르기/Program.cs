6ad778f7c18ed3bd49889765095deac9dec5880a
jacksimuse <cjh930306@naver.com>
2021-04-02 00:12:05 +0900

    2단계 if문

---
+++ Algorithm/0332/재훈/2단계 if문/사분면고르기/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 사분면고르기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 숫자를 2개 받아온다
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (a > 0 && b > 0)
+                Console.WriteLine("1"); // 둘다 양수면 1사분면
+            else if (a < 0 && b > 0)
+                Console.WriteLine("2"); // 둘다 양수면 2사분면
+            else if (a < 0 && b < 0)
+                Console.WriteLine("3"); // 둘다 양수면 3사분면
+            else if (a > 0 && b < 0)
+                Console.WriteLine("4"); // 둘다 양수면 4사분면
+        }
+    }
+}
---
+++ Algorithm/0332/재훈/2단계 if문/윤년/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 윤년
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 숫자를 받아온다
+            int yoonnyeon = int.Parse(Console.ReadLine());
+
+            if ((yoonnyeon % 4 == 0) && (yoonnyeon % 100 != 0) // 4의 배수이고 100의 배수가 아님
+                || (yoonnyeon % 400 == 0)) // 또는 400의 배수
+                Console.WriteLine("1");
+            else
+                Console.WriteLine("0");
+
+        }
+    }
+}
