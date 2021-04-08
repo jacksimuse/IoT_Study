3335a0c0959e7e89d1df291d7e2093c4dfda4eb5
guemin96 <guemin12@naver.copm>
2021-04-08 10:47:28 +0900

    커밋

--- Python/0407/박규민/210407연습.py
+++ Python/0407/박규민/210407연습.py
@@ -21,3 +21,32 @@ print(a)
 t1=('1','2','3')
 t2=t1*2
 print(len(t2))
+
+
+s1 = set([1,2,3])
+print(s1)
+# set안에 숫자랑 문자를 같이 넣는 방법?
+s2 = set("print")
+l1 = list(s2)
+print(l1[0])
+#list를 설정해주지 않으면 인덱스 사용 불가, 
+#set함수는 중복 허용X, 순서가 없기 때문에
+P1=s1&s2
+P2=s1|s2
+P3=s1-s2
+print(P1)#교집합
+print(P2)#합집합
+print(P3)#차집합
+#set 자료형에 값 추가
+s2.add(1) # 한개의 값
+s2.update([4,5,6]) # 여러개의 값
+#set 자료형에 값 삭제
+s2.remove('p')
+s2.remove(4)
+print(id(s2))
+
+
+
+#bool 형식
+#"",[],{},() 괄호가 비어있을경우에는 false가 된다
+#1은 참,0은 거짓
