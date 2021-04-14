60338e887f5c405df2d04d040ad33b746e930c0d
sumin2123 <mini247269@naver.com>
2021-04-14 17:25:03 +0900

    커밋

---
+++ Python/0414/수민/python.py
@@ -0,0 +1,21 @@
+class Calculator:
+    def __init__(self):
+        self.value = 0
+
+    def add(self, val):
+        self.value += val
+
+class MaxLimitCalculator(Calculator):
+    def add(self,val):
+        self.value += val
+        if self.value > 100:
+            self.value = 100
+            return self.value
+        else:
+            return self.value
+
+cal = MaxLimitCalculator()
+cal.add(50) # 50 더하기
+cal.add(60) # 60 더하기
+
+print(cal.value) # 100 출력
