35b347e4a9e09d5a93642a0432ddca5a065cf64d
joohy <joohyun104@gmail.com>
2021-04-16 09:01:30 +0900

    0416 커밋

---
+++ Python/0416/1차 2급 1_initial_code.py
@@ -0,0 +1,14 @@
+#You may use import as below.
+#import math
+
+def solution(shirt_size):
+    #Write code here.
+    answer = []
+    return answer
+
+#The following is code to output testcase.
+shirt_size = ["XS", "S", "L", "L", "XL", "S"]
+ret = solution(shirt_size);
+
+#Press Run button to receive output.
+print("Solution: return value of the function is ", ret, " .")
