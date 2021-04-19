66f3dcc76f6e5ce8515c5f36ece3a1dec757f5d6
joohy <joohyun104@gmail.com>
2021-04-19 17:14:59 +0900

    주현

--- Python/0419/주현/1차 2급 5_initial_code.py
+++ Python/0419/주현/1차 2급 5_initial_code.py
@@ -1,6 +1,6 @@
 def solution(arr):
     left, right = 0, len(arr)-1
-    while @@@:
+    while left < right:
         arr[left], arr[right] = arr[right], arr[left]
         left += 1
         right -= 1
