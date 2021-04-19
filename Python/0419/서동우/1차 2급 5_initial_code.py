04c033305806844c5f338ef2df238707c94e86fc
SeoDongWoo1216 <dw6642@naver.com>
2021-04-19 17:15:41 +0900

    0419 문제 커밋

---
+++ Python/0419/서동우/1차 2급 5_initial_code.py
@@ -0,0 +1,16 @@
+def solution(arr):
+    left, right = 0, len(arr)-1
+    while (left<right):
+        arr[left], arr[right] = arr[right], arr[left]
+        left += 1
+        right -= 1
+    return arr
+
+#The following is code to output testcase.
+arr = [1, 4, 2, 3]
+ret = solution(arr)
+
+#Press Run button to receive output.
+print("Solution: return value of the function is ", ret, " .")
+
+# 1 4 2 3 이 3 2 4 1 로 나와야함
