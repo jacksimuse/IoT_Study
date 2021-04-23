8bc2f45af9ac169899059066b5cf66618b2ff9c2
SeoDongWoo1216 <dw6642@naver.com>
2021-04-23 11:08:20 +0900

    0423 문제 커밋

---
+++ Python/0423/1차 2급 10_initial_code.py
@@ -0,0 +1,22 @@
+def solution(data):
+    total = sum(data)
+    average = len(data) / total
+    cnt = 0
+    for d in data:
+        if d <= average:
+            cnt += 1
+    return cnt
+
+
+#The following is code to output testcase. The code below is correct and you shall correct solution function.
+data1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ret1 = solution(data1)
+
+#Press Run button to receive output.
+print("Solution: return value of the function is", ret1, ".")
+    
+data2 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 10]
+ret2 = solution(data2)
+
+#Press Run button to receive output.
+print("Solution: return value of the function is", ret2, ".")
---
+++ Python/0423/1차 2급 9_initial_code.py
@@ -0,0 +1,14 @@
+def solution(characters):
+    result = ""
+    result += characters[0]
+    for i in range(len(characters)):
+        if characters[i - 1] != characters[i]:
+            result += characters[i]
+    return result
+
+#The following is code to output testcase. The code below is correct and you shall correct solution function.
+characters = "senteeeencccccceeee"
+ret = solution(characters)
+
+#Press Run button to receive output.
+print("Solution: return value of the function is", ret, ".")
